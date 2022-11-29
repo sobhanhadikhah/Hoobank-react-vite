1d5de78f40a6f9f5990d63bd0b722b835cd30c5e
sobhanhadikhah <hadikhahs@gmail.com>
2022-11-29 22:18:01 +0330

    we are winner

--- myappfuckung/src/components/FeedbackCard.jsx
+++ myappfuckung/src/components/FeedbackCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
-const FeedbackCard = () => {
+import { quotes } from '../assets'
+const FeedbackCard = ({ content, name, title, img }) => {
   return (
-    <div>FeedbackCard</div>
+    <div className='flex justify-between flex-col sm:flex-row px-10 py-12 max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card rounded-[20px] ' >
+      <img src={quotes} alt="quotes" className='w-[42px] h-[27px] object-contain ' />
+      <p>
+        {content}
+      </p>
+    </div>
   )
 }
 
--- myappfuckung/src/components/Testimontns.jsx
+++ myappfuckung/src/components/Testimontns.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
-
+import { card } from '../assets';
+import { feedback } from '../constanst';
+import styles, { layout } from "../constanst/style";
+import FeedbackCard from "./FeedbackCard"
 const Testimontns = () => (
-    <div>Testimontns</div>
+    <section id='clinet' className={`${styles.padding} ${styles.flexCenter} flex-col relative `} >
+        {/* TO DO FOR GRADIENT */}
+        <div />
+        <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1] '  >
+            <h1 className={styles.heading2} >
+                Lorem ipsum dolor sit, <br className='sm:block hidden' />  Eveniet, expedita!
+            </h1>
+            <div className='w-full md:mt-0 md-6 ' >
+                <p className={`${styles.paragraph} max-w-[450px] text-left `} >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem suscipit placeat tempora harum optio aut soluta voluptas aspernatur tenetur accusantium.
+                </p>
+            </div>
+        </div>
+        <div className='flex flex-wrap sm:justify-start w-full feedback-container z-[1] justify-center ' >
+            {feedback.map((cardinfo, index) => {
+                return (
+                    <>
+                        <FeedbackCard key={cardinfo.id} {...cardinfo} />
+                    </>
+                )
+            })}
+        </div>
+    </section>
 )
 export default Testimontns;
